9155537b504bf9972fb83f9cd809cd60add0d88e
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2021-01-19 08:42:41 -0800

    updated salinity profiles

--- Scripts/Cond_profiling_script.R
+++ Scripts/Cond_profiling_script.R
@@ -20,27 +20,50 @@ library(ggnewscale)
 library(ggrepel)
 
 
-### read in data
-CondPath<-here("Data","Cond_temp","Calibrated_files", "Profiling")
-files <- dir(path = CondPath,pattern = ".csv")
+#source the map data
+source(here("Scripts","cond_rad_maps.R"))
 
-# read in the data
-CondProfileData <- files %>%
-  map(~ read_csv(file.path(CondPath, .))) %>% 
-  #col_names = c("ID", "date","HighRange", "Temp","SpC","Salinity")
-  reduce(rbind) %>%
-  mutate(date = ymd_hms(date)) %>% # make data a date_time
-  select(date,Temp = TempInSitu, Salinity = SalinityInSitu_1pCal, Depth) %>%
-  drop_na(Salinity) %>%
-  filter(Salinity>28) %>% # remove the times the sensor jumped out of the water
-  mutate(Depth = - Depth) # make depth negative so the profiles go down
+### read in the profile data
 
+# CondProfileData<-read_csv("Data/Cond_temp/Calibrated_files/Profiling/011721_CT354_1pcal.csv")%>%
+#   mutate(date = ymd_hms(date)) %>% # make data a date_time
+#   select(date,Temp = TempInSitu, Salinity = SalinityInSitu_1pCal, Depth) %>%
+#   drop_na(Salinity) %>%
+#   filter(Salinity>28) %>% # remove the times the sensor jumped out of the water
+#   mutate(Depth = - Depth) # make depth negative so the profiles go down
+
+# find the waypoints that match with the rad data
+
+# RadData %>%
+#   mutate(ID =  case_when(
+#     between(Waypts$date, date, date+minutes(6) ~ Waypts$name
+#   )))
+CondProPath<-here("Data","Cond_temp","Calibrated_files", "Profiling")
+files <- dir(path = CondProPath,pattern = ".csv")
+# 
+# # read in the data
+ CondProfileData <- files %>%
+   map(~ read_csv(file.path(CondProPath, .))) %>% 
+   #   #col_names = c("ID", "date","HighRange", "Temp","SpC","Salinity")
+   reduce(rbind) %>%
+   mutate(date = ymd_hms(date)) %>% # make data a date_time
+   select(date,Temp = TempInSitu, Salinity = SalinityInSitu_1pCal, Depth) %>%
+   drop_na(Salinity) %>%
+   filter(Salinity>28) %>% # remove the times the sensor jumped out of the water
+   mutate(Depth = - Depth, # make depth negative so the profiles go down
+          Day = day(date),
+          Site = ifelse(Day==17, "Site C","Site D")) # pull out the day to facet by
+# 
 
 ## Plot profiles
 
 AllProfiles<-ggplot(CondProfileData, aes(x = date, y = Depth, col = Salinity))+
   geom_line()+
   labs(ylab = "Depth (m)")+
+  scale_color_gradient(low = "yellow", high = "blue") +
   theme_bw()+
-  ggsave("Output/salinityprofiles.pdf", width = 6, height = 6)
+  facet_wrap(~Site, scales = "free")+
+    ggsave("Output/salinityprofiles.pdf", width = 10, height = 5)
+
+
 
