8ad58d728be540b3869f25cb96da345f5fd9ffa2
Danielle Barnas <danielle.barnas@csun.edu>
2021-01-19 22:23:46 -1000

    upload ct338 from timeseries 1/17-1/19/21

--- Scripts/Cond_temp_1ptCal_new.R
+++ Scripts/Cond_temp_1ptCal_new.R
@@ -13,7 +13,6 @@ library(tidyverse)
 library(lubridate)
 library(gsw)
 library(here)
-library(driftR)
 
 here()
 
@@ -22,22 +21,26 @@ here()
 # File Paths and Serial Numbers
 ###################################
 
-cal.date<-'011721' # Date of logger calibration
-file.date<-'011721' # Date in the logger file's name
-Serial<-'354' # CT Probe Serial Number
+cal.date<-'0117-1921' # Date of logger calibration
+file.date<-'0117-1921' # Date in the logger file's name
+Serial<-'316' # CT Probe Serial Number
 
 ###################################
 # Date and Time
 ###################################
 ### Maintain date time format "YYYY-MM-DD HH:MM:SS"
 
-# Date of initial calibrations
-startCal1<-'2021-01-17 08:20:00' # Y-M-D H:M:S Calibration for One-Point Cal (50.0 mS/cm)
-endCal1<-'2021-01-17 08:21:00'
+# Date of pre-deployment calibrations
+startCal1<-'2021-01-17 08:18:00' # Y-M-D H:M:S Calibration for One-Point Cal (50.0 mS/cm)
+endCal1<-'2021-01-17 08:23:00'
+
+# Date of post-deployment calibrations
+startCal2<-'2021-01-19 13:38:00' # Y-M-D H:M:S Calibration for One-Point Cal (50.0 mS/cm)
+endCal2<-'2021-01-19 13:46:00'
 
 # Date of in situ logs
-Launch<-'2021-01-17 10:27:00'
-Retrieval<-'2021-01-17 13:13:00'
+Launch<-'2021-01-17 11:14:00'
+Retrieval<-'2021-01-19 10:08:00'
 
 ###################################
 # Conductivity Calibration Standards and Logging Interval
@@ -52,7 +55,7 @@ oneCal<-50000 # uS/cm
 # COMMENT OUT ONE OF THE FOLLOWING
 
 ### If pairing with Pressure/Depth Logger data
-Serial.depth<-'876' # Serial number of paired hobo pressure logger
+Serial.depth<-'872' # Serial number of paired hobo pressure logger
 
 ### If data were recorded at a consistent pressure (bar)
 #Pres_bar<-0
@@ -92,33 +95,61 @@ condLog$date<-condLog$date%>%parse_datetime(format = "%m/%d/%y %H:%M:%S %p", na 
 # Parse date filters into date and type vector types
 startCal1<-startCal1%>%parse_datetime(format = "%Y-%m-%d %H:%M:%S", na = character(),locale = default_locale(), trim_ws = TRUE)
 endCal1<-endCal1%>%parse_datetime(format = "%Y-%m-%d %H:%M:%S", na = character(),locale = default_locale(), trim_ws = TRUE)
+startCal2<-startCal2%>%parse_datetime(format = "%Y-%m-%d %H:%M:%S", na = character(),locale = default_locale(), trim_ws = TRUE)
+endCal2<-endCal2%>%parse_datetime(format = "%Y-%m-%d %H:%M:%S", na = character(),locale = default_locale(), trim_ws = TRUE)
 Launch<-Launch %>% parse_datetime(format = "%Y-%m-%d %H:%M:%S", na = character(),locale = default_locale(), trim_ws = TRUE)
 Retrieval<-Retrieval %>% parse_datetime(format = "%Y-%m-%d %H:%M:%S", na = character(),locale = default_locale(), trim_ws = TRUE)
 
 # Filter out dates
-condCal<-condCal%>%filter(between(date,startCal1,endCal1)) 
+condCal1<-condCal%>%filter(between(date,startCal1,endCal1))
+condCal2<-condCal%>%filter(between(date,startCal2,endCal2))
 condLog<-condLog%>%filter(between(date,Launch,Retrieval)) 
+condCal<-union(condCal1,condCal2) # Join calibration files together
 CT.data<-union(condCal,condLog)# Join Calibration and Logged files
 #CT.data$date<-CT.data$date - seconds(5) # offset time if necessary
 
-### 2. Calculate the non-linear temperature coefficient (alpha) [non-linear for groundwater influence]
-theta<-mean(condCal$TempInSitu)
-meanCalCond<-mean(condCal$E_Conductivity)
-Cond_Ref<-1060*theta+23500
+############################################################
+## Correct for drift between pre- and post-deployment calibrations
 
-# Calculate f25 using equation K25 = f25 * K.theta
-f25<-oneCal / mean(condCal$E_Conductivity)
-#####
+meanCalCond1<-mean(condCal1$E_Conductivity)
+meanCalCond2<-mean(condCal2$E_Conductivity)
+offsetCal<-meanCalCond1-meanCalCond2
+drift.corr=offsetCal/length(condLog$E_Conductivity)
 
-
-#temperature correction factor, f25, for the conversion of conductivity vlues from theta to 25C
-### Back calculate conductivity of sample series using f25 and equation above
-CT.data<-CT.data%>%
-  mutate(Sp_Conductance = f25 * E_Conductivity)
+condLog<-condLog%>%
+  mutate(drift.correction.new=drift.corr)%>%
+  mutate(drift.correction=cumsum(drift.correction.new))%>%
+  select(-drift.correction.new)
+condLog<-condLog%>%
+  mutate(E_Conductivity.corrected = E_Conductivity + drift.correction)
+condCal1$E_Conductivity.corrected<-condCal1$E_Conductivity
+condCal2$E_Conductivity.corrected<-condCal2$E_Conductivity + offsetCal
+condCal<-union(condCal1,condCal2) # Join calibration files together
+CT.data<-full_join(condCal,condLog)# Join Calibration and Logged files
 
 ############################################################
+## Calculate temperature correction factor (f25)
+
+# Calibration temperature and expected conductivity reading
+theta<-mean(condCal1$TempInSitu) # mean logged temperature at pre-deployment
+Cond_Ref<-1060*theta+23500 # actual calibration solution conductance at logged temperature at pre-deployment
+
+# Calculate f25 correction factor using equation K25 = f25 * K.theta
+f25<-oneCal / meanCalCond1
+
+# Back calculate conductivity of sample series using f25 and equation above
+CT.data<-CT.data%>%
+  mutate(Sp_Conductance = f25 * E_Conductivity.corrected)
+
+# linear compensation for conductivity > 10,000 uS/cm
+#alpha<-((Cond_Ref - oneCal)*100) / (oneCal * (theta - 25)) # temperature coefficient of variation 
+# Calculate Specific Conductance using the temperature coefficient alpha
+#CT.data<-CT.data%>%
+#  mutate(Sp_Conductance = E_Conductivity.corrected / (1 + (alpha / 100) * (TempInSitu - 25)))
+
 ############################################################
 # Load Pressure Data from HOBO Pressure Loggers for In Situ Practical Salinity Calculations
+
 If(Serial.depth = TRUE) {
   path.depth<-'Data/Depth/Calibrated_files'
   file.names.depth<-basename(list.files(path.depth,pattern = c(Serial.depth, file.date,"csv$"), recursive = F))
@@ -134,27 +165,18 @@ If(Serial.depth = TRUE) {
 CT.data<-CT.data%>% # amend to larger dataframe
   left_join(Depth.data,by='date')
 
-CT.data<-CT.data%>%
-  separate(col=date,into=c("date","time"),sep=" ")
-CT.data$date<-CT.data$date%>%parse_date(format = "%Y-%m-%d", na = character(),locale = default_locale(), trim_ws = TRUE)
-CT.data$time<-CT.data$time%>%parse_time(format = "%H:%M:%S", na = character(),locale = default_locale(), trim_ws = TRUE)
+############################################################
+# Calculate Salinity using gsw package for the PSS-78 equation
 
 CT.data<-CT.data%>%
-  dr_factor(corrFactor = Sp_Cond_drift.corr, dateVar = date, timeVar = time, keepDateTime = F)
-#CT.data<-CT.data%>%
-#  dr_correctOne(sourceVar = Sp_Conductance, cleanVar = Sp_Conductance.1pcal, calVal = Cond_Ref, calStd = oneCal, factorVar = Sp_Cond_drift.corr)
+  mutate(Sp_Cond_mS.cm=Sp_Conductance*0.001)%>% # Sp_Conductance in mS/cm
+  mutate(SalinityInSitu_1pCal=gsw_SP_from_C(C = Sp_Cond_mS.cm, t = TempInSitu, p=AbsPressure_bar)) # Use PSS-78 Equations for Salinity calculation
 
-CT.data<-CT.data%>%
-  mutate(Sp_Conductance.1pcal=Sp_Conductance + Sp_Cond_drift.corr)
-CT.data<-CT.data%>%
-  mutate(Sp_Cond_mS.cm=Sp_Conductance.1pcal*0.001)%>% # Sp_Conductance in mS/cm
-  mutate(SalinityInSitu_1pCal=gsw_SP_from_C(C = Sp_Cond_mS.cm, t = TempInSitu, p=AbsPressure_bar))%>% # Use PSS-78 Equations for Salinity calculation
-  unite(col=date,c('date','time'),sep=" ",remove=T)
-CT.data$date<-CT.data$date%>%parse_datetime(format = "%Y-%m-%d %H:%M:%S", na = character(),locale = default_locale(), trim_ws = TRUE)
+############################################################
+# Graph data
 
 CT.data%>%
   filter(between(date,Launch,Retrieval))%>%
-  filter(SalinityInSitu_1pCal>25)%>%
   ggplot(aes(x=date,y=SalinityInSitu_1pCal))+
   geom_line()
 
@@ -163,6 +185,5 @@ CT.data%>%
   ggplot(aes(x=date,y=TempInSitu))+
   geom_line()
 
-# Write CSV file and graph data
+# Write CSV file
 write_csv(CT.data,paste0('Data/Cond_temp/Calibrated_files/fixed_cal/',file.date,'_CT',Serial,'_1pcal.csv'))
-
