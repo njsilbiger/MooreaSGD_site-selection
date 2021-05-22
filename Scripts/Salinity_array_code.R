d53c2f2c2998af8ccc57f76bd728e95c4fddf3c8
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2021-05-22 14:25:53 -0700

    updated salinity array code

--- Scripts/Salinity_array_code.R
+++ Scripts/Salinity_array_code.R
@@ -1,4 +1,4 @@
-### Map of salinity arrays
+### Map of salinity arrays in the benthos and on the surface.
 
 ## load libraries ####
 library(tidyverse)
@@ -6,64 +6,105 @@ library(gganimate)
 library(ggmap)
 library(here)
 library(lubridate)
+library(viridis)
 
 ### Read in the data ####
 
-file_loc<-here("Output","May2021","QC", "Spatial","051621")
+# read in the benthic data
+file_loc_benthic<-here("Output","May2021","QC", "Spatial","051621")
 
-files <- dir(path = file_loc,pattern = ".csv")
+
+files_benthic <- dir(path = file_loc_benthic,pattern = ".csv")
+
+#GPS
+gps_benthic<-read_csv(here("Data","May2021","GPS","Varari_CT_Benthic_Array_051621.csv"))
 
 
-# Read in the gps data
+# read in the surface array data
+file_loc_surface<-here("Output","May2021","QC", "Spatial","051921")
 
-gps<-read_csv(here("Data","May2021","GPS","Varari_CT_Array_051621.csv"))
+files_surface <- dir(path = file_loc_surface,pattern = ".csv")
 
-# read in the cond data
-CondArrayData <- files %>%
+#GPS
+
+gps_surface<-read_csv(here("Data","May2021","GPS","Varari_CT_Surface_Array_051921.csv"))
+
+
+# read in the cond data ####
+CondArrayData_Benthic <- files_benthic %>%
   set_names()%>% # set's the id of each list to the file name
-  map_df(~ read_csv(file.path(file_loc, .)),.id = "filename") %>%
-  left_join(gps) %>%# Join the cond data with the gps data
-  filter(Serial != "CT_316") # something weird here
+  map_df(~ read_csv(file.path(file_loc_benthic, .)),.id = "filename") %>%
+  left_join(gps_benthic) %>% # Join the cond data with the gps data
+  mutate(Surf_Benth = "Benthic") %>% # add a column for benthic
+  select(date, Serial, TempInSitu, Salinity_off, Surf_Benth, Latitude, Longitude)
+
+## Read in the surface data 
+CondArrayData_Surface <- files_surface %>%
+  set_names()%>% # set's the id of each list to the file name
+  map_df(~ read_csv(file.path(file_loc_surface, .)),.id = "filename") %>%
+  left_join(gps_surface) %>% # Join the cond data with the gps data
+  mutate(Surf_Benth = "Surface") %>% # add a column for surface 
+  select(date, Serial, TempInSitu, Salinity_off, Surf_Benth, Latitude, Longitude ) %>%
+ mutate(Salinity_off = ifelse(Serial == "CT_354" & Salinity_off < 33, NA, Salinity_off ) ) # remove weird spike that isnt real
 
 
-# get hourly averages
-CondArrayData_hourly<- CondArrayData %>%
-  group_by(date = floor_date(date, "hour"), Serial) %>%
-  summarise(Sal_hour = mean(Salinity, na.rm = TRUE),
-            Temp_hour = mean(TempInSitu, na.rm = TRUE),
-            lat = mean(Latitude, na.rm=TRUE),
-            long = mean(Longitude, na.rm=TRUE))
+## Bind them together
+
+CondArray_all <- bind_rows(CondArrayData_Benthic, CondArrayData_Surface) 
+  
+
+## Make a plot with benthic and surface on top of each other ####
+
+CondArray_all %>%
+  filter(Surf_Benth == "Surface") %>%
+  filter(Serial != "CT_345") %>% # was exposed during low tide
+  ggplot(aes(x = date, y = Salinity_off, color = TempInSitu, group = Serial)) +
+  geom_line()+
+  facet_wrap(~Serial)+
+  scale_color_viridis_c()+
+  labs(title = "Surface",
+       ylab = "Salinity (corrected)",
+       color = "Temperature")+
+  theme_bw()
+
+# # get hourly averages
+# CondArrayData_hourly<- CondArrayData %>%
+#   group_by(date = floor_date(date, "hour"), Serial) %>%
+#   summarise(Sal_hour = mean(Salinity, na.rm = TRUE),
+#             Temp_hour = mean(TempInSitu, na.rm = TRUE),
+#             lat = mean(Latitude, na.rm=TRUE),
+#             long = mean(Longitude, na.rm=TRUE))
 
 ### read in API key for maps
 API<-names(read_table(here("Data","API.txt")))
 register_google(key = API) ### use your own API
 
 
-M_coords<-data.frame(lon =	mean(CondArrayData$Longitude, na.rm = TRUE), lat = mean(CondArrayData$Latitude, na.rm = TRUE))
+M_coords<-data.frame(lon =	mean(CondArrayData_Benthic$Longitude, na.rm = TRUE), lat = mean(CondArrayData_Benthic$Latitude, na.rm = TRUE))
 ArrayMap1<-get_map(M_coords, maptype = 'satellite', zoom = 18)
 
-ArrayMap<- ggmap(ArrayMap1)+
-  geom_point(data = CondArrayData_hourly, mapping = aes(x=long, y=lat, color = Sal_hour), size = 2, alpha = .60)+
-  xlab("")+
-  ylab("")+
-  scale_color_gradient2(low = "red",
-                        mid = "white",
-                        high = "blue",
-                        midpoint = 32)
+# ArrayMap<- ggmap(ArrayMap1)+
+#   geom_point(data = CondArrayData_hourly, mapping = aes(x=long, y=lat, color = Sal_hour), size = 2, alpha = .60)+
+#   xlab("")+
+#   ylab("")+
+#   scale_color_gradient2(low = "red",
+#                         mid = "white",
+#                         high = "blue",
+#                         midpoint = 32)
 
 # make an animation over time
-ArrayMap +
-transition_time(date) +
-  labs(title = "Time: {frame_time}")
+# ArrayMap +
+# transition_time(date) +
+#   labs(title = "Time: {frame_time}")
 
 
 # Flat map of the min values
 
-CondArrayData_hourly %>%
-  ggplot(aes(x = date, y = Sal_hour, color = lat, group = Serial)) +
+CondArrayData_Benthic %>%
+  ggplot(aes(x = date, y = Salinity, color = TempInSitu, group = Serial)) +
   geom_line() +
   facet_wrap(~Serial)+
-  ggsave(here("Output", "May2021", "SalArrayTime_hour.png"))
+  ggsave(here("Output", "May2021", "SalArrayTime.png"), width = 7, height = 6)
 
 CondArrayData_hourly %>%
   ggplot(aes(x = date, y = Temp_hour, color = lat, group = Serial)) +
