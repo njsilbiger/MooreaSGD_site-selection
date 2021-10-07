a245980ebc54e4cd6dab53dd1860794da9364e60
Silbiger <nyssa.silbiger@csun.edu>
2021-10-07 14:17:20 -0700

    added kriging maps for del15N

--- Scripts/SnapNutrientScript.R
+++ Scripts/SnapNutrientScript.R
@@ -18,7 +18,7 @@ library(wql)
 ### read in data ####
 NutData<-read_csv(here("Data","May2021", "Nutrients", "Nutrient_Snap_May2021.csv"))
 Locations<-read_csv(here("Data","May2021","GPS","Experimental_Locations", "Sandwich_Locations.csv"))
-
+TurbData<-read_csv(here("Data","August2021", "Nutrients", "Turb_NC.csv"))
 
 #### convert cond and temp to salinity
 
@@ -54,8 +54,21 @@ p3<-NutData %>%
 
 NutData %>%
   right_join(Locations) %>%
+  right_join(TurbData)%>%
   filter(Phosphate<0.6) %>%
     ggplot(aes(x = Phosphate, y = Nitrite)) +
+  geom_point()+
+  geom_smooth(method="lm")+
+  facet_wrap(~Location, scales = "free")+
+  theme_bw()
+
+
+NutData %>%
+  right_join(Locations) %>%
+  left_join(TurbData)%>%
+  #drop_na()%>%
+  #filter(Phosphate<0.6) %>%
+  ggplot(aes(x = Silicate, y = del15N)) +
   geom_point()+
   geom_smooth(method="lm")+
   facet_wrap(~Location, scales = "free")+
@@ -85,7 +98,8 @@ MeanGPS<-Locations %>%
 
 # combined data
 AllData <- NutData %>%
-  right_join(Locations) 
+  right_join(Locations) %>%
+  left_join(TurbData)
 
 ## Vaiari
 
@@ -103,6 +117,15 @@ Map_V2<-ggmap(ArrayMap1) +
   ggtitle("Varari")
 
 
+Map_V3<-ggmap(ArrayMap1) +
+  geom_point(data = AllData %>% filter(Location =="Varari"), aes(x = lon, y = lat, color = del15N), size = 4)+
+  scale_color_viridis_c(trans = "sqrt", option = "plasma")+
+  ggtitle("Varari")
+
+Map_V4<-ggmap(ArrayMap1) +
+  geom_point(data = AllData %>% filter(Location =="Varari"), aes(x = lon, y = lat, color = N_percent), size = 4)+
+  scale_color_viridis_c(trans = "sqrt", option = "plasma")+
+  ggtitle("Varari")
 
 
 # Cabral
@@ -120,6 +143,17 @@ Map_C2<-ggmap(ArrayMap2)+
   ggtitle("Cabral (370-431 uM at seep)")
 
 
+Map_C3<-ggmap(ArrayMap2) +
+  geom_point(data = AllData %>% filter(Location =="Cabral"), aes(x = lon, y = lat, color = del15N), size = 4)+
+  scale_color_viridis_c(trans = "sqrt", option = "plasma")+
+  ggtitle("Cabral")
+
+Map_C4<-ggmap(ArrayMap2) +
+  geom_point(data = AllData %>% filter(Location =="Cabral"), aes(x = lon, y = lat, color = N_percent), size = 4)+
+  scale_color_viridis_c(trans = "sqrt", option = "plasma")+
+  ggtitle("Cabral")
+
+
 (Map_V1 + Map_V2)/(Map_C1 + Map_C2) 
 
 
@@ -134,19 +168,25 @@ C_kml <- getKMLcoordinates(kmlfile=here("Data","Polygons","Cabral_Polygon.kml"),
 # Run the kriging
 
 VData <- AllData %>%
-  filter(Location == 'Varari')
+  filter(Location == 'Varari') %>%
+  drop_na(Silicate)
 
 krig1 <- kriging(VData$lon, VData$lat, VData$Silicate, pixels=500,polygons=V_kml) ###pixels controls how fine or course you want the prediction data frame to be
 krig2 <- krig1$map
 
+krig1_del15N <- kriging(VData$lon, VData$lat, VData$del15N, pixels=500,polygons=V_kml) ###pixels controls how fine or course you want the prediction data frame to be
+krig2_del15N2 <- krig1_del15N$map
+
 
 CData <- AllData %>%
   filter(Location == 'Cabral') %>%
-  select(Top_Plate_ID, lon, lat, Phosphate, Silicate, Nitrite, Ammonia)
+  select(CowTagID, lon, lat, Phosphate, Silicate, Nitrite, Ammonia, del15N, N_percent)
 
 krig3 <- kriging(CData$lon, CData$lat, CData$Silicate, pixels=500,polygons=C_kml, lags = 3) ###pixels controls how fine or course you want the prediction data frame to be
 krig4 <- krig3$map
 
+krig3_del15N <- kriging(CData$lon, CData$lat, CData$del15N, pixels=500,polygons=C_kml, lags = 3) ###pixels controls how fine or course you want the prediction data frame to be
+krig4_del15N2 <- krig3_del15N$map
 
 
 ## make the map
@@ -165,8 +205,26 @@ V_krig_map<-ggmap(ArrayMap1)+
   theme(panel.grid.major = element_line(color = 'white', linetype = "dashed",size = 0.5),
         plot.background=element_rect(fill='white'))+
   ggtitle("Varari")
-
 ggsave(plot = V_krig_map, filename = here("Output","May2021","Spatial_array_plots","v_krig_map.png"))
+
+# del15N
+V_krig_map_del15N<-ggmap(ArrayMap1)+
+  geom_point(data=krig2_del15N2, aes(x=x, y=y, colour=pred), size=4, alpha=0.5) + 
+  geom_point(data = VData, aes(x=lon, y=lat))+
+  scale_color_viridis_c(name = "del15N", option = "rocket")+
+  coord_sf() +
+  theme(axis.line=element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank()) +  
+  theme(panel.grid.major = element_line(color = 'white', linetype = "dashed",size = 0.5),
+        plot.background=element_rect(fill='white'))+
+  ggtitle("Varari")
+
+
+ggsave(plot = V_krig_map_del15N, filename = here("Output","May2021","Spatial_array_plots","v_krig_map_del15N.png"))
 
 # Cabral
 C_krig_map<-ggmap(ArrayMap2)+
@@ -185,3 +243,25 @@ C_krig_map<-ggmap(ArrayMap2)+
   ggtitle("Cabral")
 
 ggsave(plot = C_krig_map, filename = here("Output","May2021","Spatial_array_plots","C_krig_map.png"))
+
+
+C_krig_map_del15N<-ggmap(ArrayMap2)+
+  geom_point(data=krig4_del15N2, aes(x=x, y=y, colour=pred), size=4, alpha=0.5) + 
+  geom_point(data = CData, aes(x=lon, y=lat))+
+  scale_color_viridis_c(name = "del15N", option = "rocket")+
+  coord_sf() +
+  theme(axis.line=element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank()) +  
+  theme(panel.grid.major = element_line(color = 'white', linetype = "dashed",size = 0.5),
+        plot.background=element_rect(fill='white'))+
+  ggtitle("Cabral")
+
+ggsave(plot = C_krig_map_del15N, filename = here("Output","May2021","Spatial_array_plots","C_krig_map_del15N.png"))
+
+
+
+(V_krig_map + V_krig_map_del15N)/(C_krig_map+C_krig_map_del15N)
