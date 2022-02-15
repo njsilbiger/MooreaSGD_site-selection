153043f3983339199574e89aa0ad1dce181aedc4
Silbiger <nyssa.silbiger@csun.edu>
2022-02-15 12:30:31 -0800

    updated pH seep data to be on total scale without salinity correction

--- Scripts/Tris_hobo_pH_Slope.R
+++ Scripts/Tris_hobo_pH_Slope.R
@@ -4,41 +4,143 @@ library(tidyverse)
 library(seacarb)
 library(broom)
 library(here)
-here()
+library(lubridate)
+library(gsw)
+library(gridExtra)
+library(mooreasgd)
 
 
-foldername<-'Data/20201028/'
-filename<-'pH_196_2020-11-12.csv'
+
+
+
+###################################
+
+### Input
+# Path to folder storing logger .csv files
+path.log<-here("Data","August2021","Varari_Sled", "20210804","raw_files") # Logger in situ file path (CT and Water Level files)
+file.date <- "20210804" # logger date used in file name(s)
+
+### Output
+# Path to store logger files
+path.output<-here("Data","August2021","Varari_Sled", "20210804","QC_files") # Output file path
+
+
+###################################
+### Logger Serial Numbers
+###################################
+
+pH_Serial <- "197"
+
+###################################
+### Logger Launch and Retrieval dates
+###################################
+
+# Log dates
+start.date <- ymd('2021-08-04')
+end.date <- ymd('2021-8-08')
+
+# do you want to plot a graph?
+plotgraph<-'no'
+
+###################################
+### Import calibration and launch records
+###################################
+
+# Read in files that are updated with calibration and launch information
+#calibration.log<-read_csv(here("Data","Tris_Calibration.csv")) # Calibration time logs
+launch.log<-read_csv(here("Data","Launch_Log.csv")) %>%  # Launch time logs
+  filter(Log_Type == "PH")
+
 
 ## bring in pH calibration files and raw data files
-pHcalib<-read_csv('Probe_and_Logger_Protocols/HOBO_pH_Logger/Data/Tris_Calibration_Log.csv')
-pHData<-read_csv(paste0(foldername,filename))
+pHcalib<-read_csv(here("Data","August2021","CarbonateChemistry","TrisCalibrationLog.csv")) %>%
+  mutate(TrisCalDate = mdy(TrisCalDate))
+
+
 
 Salinity_lab<-34 # normally would be a column in pHData datasheet, from CT_Logger
 
+#################################################################################
+# DO NOT CHANGE ANYTHING BELOW HERE ----------------------------------
+#################################################################################
+
+############################################################
+### Read in Logger Files
+############################################################
+
+# cleanup function pulled from 'mooreasgd' package
+# Reads in raw csv and returns tidied csv for the probe with the specified serial number
+
+# In Situ pH file
+pH.data <- pH_cleanup(data.path = path.log, pH.serial = pH_Serial) %>% 
+  rename(pH_Serial = Serial)
+
+############################################################
+### Parse date and time
+############################################################
+
+### Parse dates into date and type vector types
+
+launch.log <- launch.log %>%
+  mutate(time_start = mdy_hm(time_start), # convert to time
+         time_end = mdy_hm(time_end),
+         start  = date(time_start), # extract the date
+         end = date(time_end)) %>%
+  filter(Serial == paste0("PH_",pH_Serial), # pulll out the right serial number
+         start == ymd(start.date),
+         end == ymd(end.date))
+
+############################################################
+### In Situ Logger Data
+############################################################
+pHLog<-pH.data %>% # extract the data you need
+  filter(between(date,launch.log$time_start, launch.log$time_end))
+
+
 # Convert 'date' to date and time vector type
-pHcalib$date<-pHcalib$date %>%
-  parse_datetime(format = "%m/%d/%y", na = character(),
-                 locale = default_locale(), trim_ws = TRUE)
+# filter just the HOBO calibration
+pHcalib<-pHcalib %>%
+  filter(HOBO_Orion == "HOBO")
+
+# 
+# pHcalib$TrisCalDate<-pHcalib$TrisCalDate %>%
+#   parse_datetime(format = "%m/%d/%y", na = character(),
+#                  locale = default_locale(), trim_ws = TRUE)
 
 ## take the mV calibration files by each date and use them to calculate pH
 pHSlope<-pHcalib %>%
-  dplyr::nest_by(date) %>%
+  dplyr::nest_by(TrisCalDate) %>%
   dplyr::mutate(fitpH = list(lm(mVTris~TTris, data = data))) %>% # linear regression of mV and temp of the tris
   dplyr::summarise(broom::tidy(fitpH)) %>% # make the output tidy
-  select(date, term, estimate) %>%
+  select(TrisCalDate, term, estimate) %>%
   pivot_wider(names_from = term, values_from = estimate) %>%# put slope and intercept in their own column
-  full_join(pHData) %>% # join with the pH sample data
-  separate(date,sep=" ",into=c('date','time')) %>%
-  fill('(Intercept)',TTris) %>%
+#  full_join(pHData) %>% # join with the pH sample data
+#  separate(date,sep=" ",into=c('date','time')) %>%
+  bind_cols(pHLog)%>%
+ # fill('(Intercept)',TTris) %>%
   drop_na() %>%
   mutate(mVTris = TempInSitu*TTris + `(Intercept)`) %>% # calculate the mV of the tris at temperature in which the pH of samples were measured
-  mutate(pHcal = pH(Ex=mV,Etris=mVTris,S=Salinity_lab,T=TempInSitu)) %>% # calculate pH of the samples using the pH seacarb function
+  mutate(pH_total = pH(Ex=mV,Etris=mVTris,S=Salinity_lab,T=TempInSitu)) %>% # calculate pH of the samples using the pH seacarb function
   #mutate(pH_insitu = pHinsi(pH = pH, ALK = TA_Raw, Tinsi = TempInSitu, Tlab = Temp, S = Salinity_lab_Silbiger)) %>%
-  select(date,time,TempInSitu,pH,mV,pHcal,mVTris) %>% # ,Salinity_lab ## need to calculate pH insi then it is done
-  unite(date,c(date,time),sep=" ")
+  select(date,TempInSitu,pH,mV,pH_total,mVTris) 
 
 View(pHSlope)
 
+
+
+if(plotgraph=='yes'){
+  # Plot pH data
+  p<-list()
+  p[[1]]<-pHSlope %>% 
+    ggplot(aes(x = date, y = pH_total, color = TempInSitu)) + 
+    geom_line() + 
+    theme_bw() +
+    labs(x = "Date", color = "Temperature (C)") +
+    ggtitle(paste("pH",pH_Serial))
+}
+  
 ## write the data
-write_csv(pHSlope, paste0(foldername,'TCal_',filename))
+# write out the clean data
+write_csv(pHLog, paste0(path.output,"/QC_pH_",file.date,"_pHtot_",pH_Serial,".csv"))
+
+
