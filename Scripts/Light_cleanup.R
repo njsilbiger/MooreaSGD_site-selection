e4e5a50ec15ad2840b11306fac1e0a49c8ea970f
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2022-04-28 18:53:24 -0700

    added lux data for most of cabral sled

--- Scripts/Light_cleanup.R
+++ Scripts/Light_cleanup.R
@@ -33,27 +33,27 @@ library(mooreasgd)
 
 ### Input
 # Path to folder storing logger .csv files
-path.log<-here("Data","March2022","Varari_Sled","20220404","raw_files", "lux") # Logger in situ file path (CT and Water Level files)
-file.date <- "20220404" # logger date used in file name(s)
+path.log<-here("Data","March2022","Cabral_Sled","20220329","raw_files", "lux") # Logger in situ file path (CT and Water Level files)
+file.date <- "20220328" # logger date used in file name(s)
 
 ### Output
 # Path to store logger files
-path.output<-here("Data","March2022","Varari_Sled","20220404","QC_files") # Output file path
+path.output<-here("Data","March2022","Cabral_Sled","20220329","QC_files") # Output file path
 
 
 ###################################
 ### Logger Serial Numbers
 ###################################
 
-Light_Serial <- "848"
+Light_Serial <- "838"
 
 ###################################
 ### Logger Launch and Retrieval dates
 ###################################
 
 # Log dates
-start.date <- ymd('2022-04-04')
-end.date <- ymd('2022-04-14')
+start.date <- ymd('2022-03-29')
+end.date <- ymd('2022-03-31')
 
 # do you want to plot a graph?
 plotgraph<-'no'
