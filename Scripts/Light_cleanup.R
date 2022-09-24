154307346951f915c11cf569caea6bfc11080677
Maya Greenhill <mayagreenhill@gmail.com>
2022-09-24 12:48:03 -0700

    added QC light data to varari sled 20220717

--- Scripts/Light_cleanup.R
+++ Scripts/Light_cleanup.R
@@ -33,12 +33,12 @@ library(mooreasgd)
 
 ### Input
 # Path to folder storing logger .csv files
-path.log<-here("Data","June2022","Varari_Sled","20220627","raw_files", "Lux") # Logger in situ file path (CT and Water Level files)
-file.date <- "20220627" # logger date used in file name(s)
+path.log<-here("Data","June2022","Varari_Sled","20220717","raw_files", "Lux") # Logger in situ file path (CT and Water Level files)
+file.date <- "20220717" # logger date used in file name(s)
 
 ### Output
 # Path to store logger files
-path.output<-here("Data","June2022","Varari_Sled","20220627","QC_files") # Output file path
+path.output<-here("Data","June2022","Varari_Sled","20220717","QC_files") # Output file path
 
 
 ###################################
@@ -52,8 +52,8 @@ Light_Serial <- "858"
 ###################################
 
 # Log dates
-start.date <- ymd('2022-06-27')
-end.date <- ymd('2022-07-02')
+start.date <- ymd('2022-07-17')
+end.date <- ymd('2022-07-22')
 
 # do you want to plot a graph?
 plotgraph<-'yes'
