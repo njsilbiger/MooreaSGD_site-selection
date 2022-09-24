3550cb3c36dc39040956284379c5654f7df16d41
Maya Greenhill <mayagreenhill@gmail.com>
2022-09-24 13:17:28 -0700

    added QC pH data to varari_sled 20220613

--- Scripts/pH_Processing.R
+++ Scripts/pH_Processing.R
@@ -30,12 +30,12 @@ library(mooreasgd)
 
 ### Input
 # Path to folder storing logger .csv files
-path.log<-here("Data","June2022","Varari_Sled", "20220607","raw_files") # Logger in situ file path (CT and Water Level files)
-file.date <- "20220607" # logger date used in file name(s)
+path.log<-here("Data","June2022","Varari_Sled", "20220613","raw_files") # Logger in situ file path (CT and Water Level files)
+file.date <- "20220613" # logger date used in file name(s)
 
 ### Output
 # Path to store logger files
-path.output<-here("Data","June2022","Varari_Sled", "20220607","QC_files") # Output file path
+path.output<-here("Data","June2022","Varari_Sled", "20220613","QC_files") # Output file path
 
 
 ###################################
@@ -49,8 +49,8 @@ pH_Serial <- "196"
 ###################################
 
 # Log dates
-start.date <- ymd('2022-06-07')
-end.date <- ymd('2022-06-11')
+start.date <- ymd('2022-06-13')
+end.date <- ymd('2022-06-19')
 
 # do you want to plot a graph?
 plotgraph<-'yes'
