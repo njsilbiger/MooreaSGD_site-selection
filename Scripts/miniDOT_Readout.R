b9a4afe8ab36d384f79e740987051837c15f89df
Maya Greenhill <mayagreenhill@gmail.com>
2022-10-06 14:10:26 -0700

    added remainder of QC minidot files to June2022 Varari sled

--- Scripts/miniDOT_Readout.R
+++ Scripts/miniDOT_Readout.R
@@ -11,30 +11,30 @@ library(here)
 
 ### Input
 # Path to folder storing logger .csv files
-path.log<-here("Data","March2022","Cabral_Sled","20220331","raw_files", "miniDOT") # Logger in situ file path 
-file.date <- "202203131" # logger date used in file name(s)
+path.log<-here("Data","June2022","Varari_Sled","20220717","raw_files", "miniDOT") # Logger in situ file path 
+file.date <- "20220717" # logger date used in file name(s)
 
 ### Output
 # Path to store logger files
-path.output<-here("Data","March2022","Cabral_Sled","20220331","QC_files") # Output file path
+path.output<-here("Data","June2022","Varari_Sled","20220717","QC_files") # Output file path
 
 
 ###################################
 ### Logger Serial Numbers
 ###################################
 
-DO_Serial <- "719"
+DO_Serial <- "496"
 
 ###################################
 ### Logger Launch and Retrieval dates
 ###################################
 
 # Log dates
-start.date <- ymd('2022-03-31')
-end.date <- ymd('2022-04-03')
+start.date <- ymd('2022-07-17')
+end.date <- ymd('2022-07-22')
 
 # do you want to plot a graph?
-plotgraph<-'no'
+plotgraph<-'yes'
 
 ##############################
 ## DO NOT CHANGE ANYTHING BELOW
